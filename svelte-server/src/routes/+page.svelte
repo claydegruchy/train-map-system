fd74a7ce4a29a7f4692d0933fa33e6fe8758ad0a
Clay DeGruchy <clay.degruchy+github@gmail.com>
2024-01-03 14:21:14 +0100

    got basics working working

--- svelte-server/src/routes/+page.svelte
+++ svelte-server/src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+
+    import Graph from "../components/Graph.svelte";
+    import GraphNode from "../components/GraphNode.svelte";
+    import GraphEdge from "../components/GraphEdge.svelte";
+
+    const nodes = [
+        { id: "N1", label: "Start" },
+        { id: "N2", label: "4" },
+        { id: "N4", label: "8" },
+        { id: "N5", label: "15" },
+        { id: "N3", label: "16" },
+        { id: "N6", label: "23" },
+        { id: "N7", label: "42" },
+        { id: "N8", label: "End" },
+    ];
+
+    const edges = [
+        { id: "E1", source: "N1", target: "N2" },
+        { id: "E2", source: "N2", target: "N3" },
+        { id: "E3", source: "N3", target: "N6" },
+        { id: "E4", source: "N2", target: "N4" },
+        { id: "E5", source: "N4", target: "N5" },
+        { id: "E6", source: "N5", target: "N4", label: "2" },
+        { id: "E7", source: "N5", target: "N6" },
+        { id: "E8", source: "N6", target: "N7" },
+        { id: "E9", source: "N7", target: "N7", label: "3" },
+        { id: "E10", source: "N7", target: "N8" },
+    ];
+</script>
+
+<body>
+    <h1>Graph</h1>
+    <Graph>
+        {#each nodes as node}
+            <GraphNode {node} />
+        {/each}
+
+        {#each edges as edge}
+            <GraphEdge {edge} />
+        {/each}
+    </Graph>
+</body>
