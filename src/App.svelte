e3eea5c34a438c4640c19b2af293561b6d5030d9
Clay DeGruchy <clay.degruchy+github@gmail.com>
2025-03-31 15:28:56 +0200

    added help, removed generators

--- src/App.svelte
+++ src/App.svelte
@@ -12,38 +12,7 @@
 
   import db from "./lib/db.json";
   import loc from "./lib/loc.json";
-
-  async function getCoordinates(town) {
-    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(town)}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data.length > 0) {
-      return [parseFloat(data[0].lon), parseFloat(data[0].lat)]; // [lng, lat]
-    }
-    return null; // 否 results found
-  }
-
-  async function download() {
-    let locations = {};
-    for (const route of db) {
-      console.log(route);
-
-      for (const { Stops } of route.SplitRoutes) {
-        for (const stop of Stops) {
-          if (stop == "non-stop") continue;
-          if (locations[stop]) continue;
-          console.log(stop);
-          locations[stop] = true;
-        }
-      }
-    }
-
-    for (const loc of Object.keys(locations)) {
-      locations[loc] = await getCoordinates(loc);
-      console.log(locations);
-    }
-    console.log(locations);
-  }
+  import Dialog from "./lib/Blocks/Dialog.svelte";
 
   function generateLine(route) {
     console.log(route);
@@ -89,8 +58,6 @@
         }
       }
     }
-
-    console.log(db);
   });
 
   function stringToAscii(str) {
@@ -109,6 +76,46 @@
   let selectedStation = null;
 </script>
 
+<nav class="top right">
+  <Dialog>
+    <div slot="button">What is this?</div>
+    <div slot="content">
+      <h2>What is this?</h2>
+      <p>
+        This is a map of all night trains in europe as assembled by <a
+          href="back-on-track.eu">back-on-track.eu</a
+        >. All work for the data behind this map goes to back-on-track (I just
+        reprocessed it into a map that I find easier to use)
+      </p>
+      <h3>How do I use this?</h3>
+      <div>
+        <ul>
+          <li>
+            Clicking on a <b>route</b> will isolate it and show info about the route
+            and it's stops.
+          </li>
+          <li>
+            Clicking on a <b>station</b> will isolate it and show all routes leaving
+            from that stop.
+          </li>
+        </ul>
+      </div>
+      <h3>Why did you make this?</h3>
+      <p>
+        I found the map layout of back-on-track to be hard to navigate, as the
+        london tube style of train map doesn't scale as geographic relativity
+        becomes more important
+      </p>
+      <h3>
+        Whats with the fucked up long spikey lines? Do I really need to go to
+        south america to get from Tarbes – Paris?
+      </h3>
+      <p>Some coordinates didn't map properly, they'll be sorted out soon.</p>
+      <h3>I think this looks like shit</h3>
+      <p>well thats just your opinion, man</p>
+    </div>
+  </Dialog>
+</nav>
 <MapLibre
   center={[-0.118092, 51.509865]}
   zoom={3}
@@ -119,6 +126,7 @@
   {#each db as route}
     <GeoJSON id={route.Itinerary} data={generateLine(route)}>
       <LineLayer
+        hoverCursor="pointer"
         layout={{ "line-cap": "round", "line-join": "round" }}
         paint={{
           "line-width":
@@ -148,7 +156,7 @@
   <ZoomRange minzoom={7 + (selectedStation ? -3 : 0)} enforce>
     {#each Object.entries(locations) as [k, { name, lngLat }]}
       {#if lngLat}
-        {#if !selectedRoute || db.filter((r) => r.Itinerary == selectedRoute && r.stations.includes(name)).length > 0}
+        {#if !selectedRoute || (selectedRoute && db.filter((r) => r.Itinerary == selectedRoute && r.stations.includes(name)).length > 0)}
           <Marker {lngLat}>
             <div class="marker">{name}</div>
             <Popup
